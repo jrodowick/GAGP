c6e49731df35ccc957b3d0ed3fabce8be8f0b5a8
jrodowick <jrodowick@mail.csuchico.edu>
2018-09-09 16:43:30 -0700

    Formats landing page/Adds login/logout feature/Adds text to index.html

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    re_path('^$', views.index, name = 'index'),
+    path('register/', views.register, name = 'register'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'index.html'), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from .forms import *
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if(request.method == 'POST'):
+        form = RegistrationForm(request.POST)
+        if(form.is_valid()):
+            user = form.save(commit = True)
+
+            if(user is not None):
+                login(request, user)
+            return redirect('/')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'register.html', {'form':form})
