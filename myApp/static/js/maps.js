eb3a573f8d80854a81a26843d554b7e8aa88764f
jrodowick <jrodowick@mail.csuchico.edu>
2018-09-16 15:59:30 -0700

    Adds info panels to all markers on map

--- myApp/static/js/maps.js
+++ myApp/static/js/maps.js
@@ -10,35 +10,40 @@
 
 var geocoder;
 var map;
-var myLatLng = {lat:39.649885, lng:-121.567749};
+var myLatLng = {lat:39.7285, lng:-121.8375};
 
 function initMap() {
   // alert('You in the initMap()')
         geocoder =  new google.maps.Geocoder();
         map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
+          zoom: 11,
           center: myLatLng
         });
         var locDiv = document.getElementById('locations');
         var locations = locDiv.getElementsByTagName('span');
-        codeAddress(locations)
+
+        var parkNames = document.getElementsByClassName('panel-heading');
+
+
+        drop(locations, parkNames)
 }
 google.maps.event.addDomListener(window, 'load', initMap);
 
 
-function drop(markerArray) {
+function drop(markerArray, parkNames) {
   for (var i =0; i < markerArray.length; i++) {
-    setTimeout(function() {
-      codeAddress(markerArray);
-    }, i * 200);
+    contentString = '<h4>' + parkNames[i].innerHTML + '</h4>' +
+                    '<a href="#">Create Event here.</a>';
+    var infoWindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth:400
+    })
+    codeAddress(markerArray[i].innerHTML, infoWindow, i*200);
   }
 }
 
-function codeAddress(address) {
-  for(i = 0; i < address.length; i++)
-  {
-    geocoder.geocode( {'address':address[i].innerHTML}, function(results, status) {
-
+function codeAddress(address, infoWindow, timeout) {
+    geocoder.geocode( {'address':address}, function(results, status) {
         setTimeout(function() {
           if (status == 'OK') {
             var marker = new google.maps.Marker({
@@ -46,9 +51,10 @@ function codeAddress(address) {
                 animation: google.maps.Animation.DROP,
                 position: results[0].geometry.location
             });
+            marker.addListener('click', function() {
+              infoWindow.open(map, marker);
+            })
           }
-        }, i * 200)
-
+        }, timeout)
     })
-  }
 }
