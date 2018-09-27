9b63b31c0f9e342cf68aba7e8a61eddd4dad3bc2
jrodowick <jrodowick@mail.csuchico.edu>
2018-09-27 12:47:59 -0700

    Adds  for event info on makrers

--- myApp/static/js/maps.js
+++ myApp/static/js/maps.js
@@ -1,16 +1,22 @@
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     LatLng = {}
-//   }
-//   methods: {
-//
-//   }
-// })
+function csrfSafeMethod(method) {
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+
+$.ajaxSetup({
+  beforeSend: function(xhr, settings) {
+    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader('X-CSRFToken', csrftoken);
+    }
+  }
+});
+
 
 var geocoder;
 var map;
 var myLatLng = {lat:39.7285, lng:-121.8375};
+
+
 
 function initMap() {
   // alert('You in the initMap()')
@@ -19,24 +25,49 @@ function initMap() {
           zoom: 11,
           center: myLatLng
         });
+        get_events();
+}
+
+function get_events() {
+  $.post('/local_event/')
+    .done(function(result) {
+      var events = result;
+      start_map(events)
+    });
+}
+
+function start_map(events) {
+
+
         var locDiv = document.getElementById('locations');
         var locations = locDiv.getElementsByTagName('span');
 
         var parkNames = document.getElementsByClassName('panel-heading');
 
 
-        drop(locations, parkNames)
+        drop(locations, parkNames, events)
 }
 google.maps.event.addDomListener(window, 'load', initMap);
 
 
-function drop(markerArray, parkNames) {
-  for (var i =0; i < markerArray.length; i++) {
-    contentString = '<h4>' + parkNames[i].innerHTML + '</h4>' +
+function drop(markerArray, parkNames, events) {
+  for (var i = 0; i < markerArray.length; i++) {
+    var event_list = ''
+    for(var e = 0; e < events.length; e++)
+    {
+      if(events[e]['location']['name'] == parkNames[i].innerHTML)
+      {
+        event_list += 'Event title: ' + events[e]['name'] + ' ' +
+                      'Type of activity: ' + events[e]['activity'] + '<br>'
+      }
+    }
+    contentString = '<h3>' + parkNames[i].innerHTML + '</h3>' +
+                    '<h4>Events at this location:<h4>' +
+                    '<h5>' + event_list + '</h5>' +
                     '<a href="/events">Create Event here.</a>';
     var infoWindow = new google.maps.InfoWindow({
       content: contentString,
-      maxWidth:400
+      maxWidth:500
     })
     codeAddress(markerArray[i].innerHTML, infoWindow, i*200);
   }
