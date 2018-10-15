7be8c095f59778c29ba404693c59e11627d3efe7
jrodowick <jrodowick@mail.csuchico.edu>
2018-10-15 10:24:10 -0700

    Reworks  call to use locations/Adds modal info

--- myApp/static/js/maps.js
+++ myApp/static/js/maps.js
@@ -25,63 +25,53 @@ function initMap() {
           zoom: 11,
           center: myLatLng
         });
+        //get_locations();
         get_events();
+
 }
 
 function get_events() {
   $.post('/local_event/')
     .done(function(result) {
-      var events = result;
-      start_map(events)
+      drop(result[0], result[1])
     });
-}
-
-function start_map(events) {
-
-
-        var locDiv = document.getElementById('locations');
-        var locations = locDiv.getElementsByTagName('span');
-
-        var parkNames = document.getElementsByClassName('card-header');
-
-
-        drop(locations, parkNames, events)
 }
 google.maps.event.addDomListener(window, 'load', initMap);
 
+function fillmodal(title) {
+    var info = title.split(",")
+    $(".modal-header .modal-title").text(info[0]);
+    $(".modal-body").text('Sport: ' + info[1] +
+                          '\nDate of event: ' + info[2] +
+                          '\nTime of event: ' + info[3])
+    $("#myModal").modal()
+}
 
-function drop(markerArray, parkNames, events) {
-  for (var i = 0; i < markerArray.length; i++) {
+function drop(locations, events) {
+  for (var i = 0; i < locations.length; i++) {
     var event_list = ''
     for(var e = 0; e < events.length; e++)
     {
-      if(events[e]['location']['name'] == parkNames[i].innerHTML)
+      if(events[e]['location']['name'] == locations[i]['name'])
       {
-        event_list +=  '<div class=container">' +
-                         '<button type="button" class="btn btn-primary" data-toggle="collapse" data-target=#' + events[e]['name'] + '>' +
+        //console.log(events)
+        event_list +=  '<div class="container">' +
+                         '<button onclick="fillmodal(\''+ events[e]['name'] + ',' + events[e]['activity'] + ',' + events[e]['date_of_event'] + ',' + events[e]['time_of_event'] +'\')"'  + '>'+
                               events[e]['name'] +
                          '</button>' +
-                          '<div id=' + events[e]['name'] + ' class="collapse">' +
-                            '<div class="container" style="border:dotted;">' +
-                              '<br><p><strong>' + 'Sport: </strong>' + events[e]['activity'] + '</p>' +
-                              '<p><strong>' + 'Event on: </strong>' + events[e]['date_of_event'] + '</p>' +
-                              '<a href="#">Join this event.</a>' +
-                            '</div>' +
-                          '</div>' +
-                        '</div>' +'<br>'
-
+                        '</div><br>';
       }
     }
-    contentString = '<h3>' + parkNames[i].innerHTML + '</h3>' +
+    contentString = '<h3>' + locations[i]['name'] + '</h3>' +
                     '<h5> Events at this location: </h5>' +
-                      event_list +
+                      event_list + '<br>' +
                     '<a href="/events">Create Event here.</a>';
     var infoWindow = new google.maps.InfoWindow({
       content: contentString,
       maxWidth:500
     })
 
-    codeAddress(markerArray[i].innerHTML, infoWindow, i*200);
+    codeAddress(locations[i]['address'], infoWindow, i*200);
   }
 }
 
@@ -101,12 +91,6 @@ function codeAddress(address, infoWindow, timeout) {
               closeMarkers(map,markers);
               infoWindow.open(map, marker);
             })
-
-
-
-            // marker.addListener('click', function() {
-            //   infoWindow.open(map, marker);
-            // })
           }
         }, timeout)
     })
@@ -117,3 +101,39 @@ function closeMarkers(map,markers) {
     marker.infowindow.close(map,marker);
   })
 }
+
+
+// (\''+ events[e] +'\')
+// $(function() {
+//         $(document).on('click','button.btn-primary',function(){
+
+//             //alert(events)
+//             $(".modal-header .modal-title").text("Title of event");
+//             $(".modal-body").text("Relevant Information")
+//             $("#myModal").modal()
+//         });
+// });
+
+
+// '<div class="modal fade" role="dialog" id=' + events[e]['name'] + '>' +
+//   '<div class="modal-dialog">' +
+//     '<div class="modal-content">' +
+//       '<div class="modal-header">' +
+//         '<h4 class="modal-title">' + events[e]['name'] + '</h4>' +
+//         '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
+//       '</div>' +
+//       '<div class="modal-body">' +
+//         '<p style="font-size:110%;"><strong>' + 'Created by: </strong>' + events[e]['created_by'] + '</p>' +
+//         '<p style="font-size:110%;"><strong>' + 'Sport: </strong>' + events[e]['activity'] + '</p>' +
+//         '<p style="font-size:110%;"><strong>' + 'Event on: </strong>' + events[e]['date_of_event'] + '</p>' +
+//         '<p style="font-size:110%;"><strong>' + 'Time: </strong>' + events[e]['time_of_event'] + '</p>' +
+//         '<a href="#">Join this event</a>' +
+//       '</div>' +
+//
+//       '<div class="modal-footer">' +
+//         '<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>' +
+//       '</div>' +
+//     '</div>' +
+//   '</div>' +
+// '</div>';
+//alert(id)
